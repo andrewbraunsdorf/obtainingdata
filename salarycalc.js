bbe72ceb14f54f61f54f117e82a7e1975bf8045f
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-17 15:27:22 +0000

    added dummy data

---
+++ salarycalc.js
@@ -0,0 +1,5 @@
+var departmentid = [d001, d002, d003, d004, d005]
+var departments =  ["Marketing", "Finance", "Human Resources", "Production", "Development"]
+var employeeid = [[100, 102, 103, 104, 104, 500], [200, 201, 202, 203, 204], [305, 306, 307], [402, 403, 404], [501, 502, 503]]
+var salaries = [[65000, 85000, 95000, 100000, 70000, 6000], [85000, 120000, 65000, 75000, 80000], [50000, 55000, 75000], [50000, 50000, 80000], [75000, 100000, 65000]
+var employeeName = [["George", "Bob", "Rob", "Fred", "Harry", "Mike"], ["Tim", "Mary", "Jin", "Liz", "Yoshi"], ["Jen", "Kay", "Ella"], ["Tom", "Syd", "Emily"], ["Cat", "Kate", "Mary"]]
