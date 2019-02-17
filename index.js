88dfb9c0ee3f9a66649aeec46dfddf94a870b383
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-17 15:21:06 +0000

    added line splits into departments

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+fs.readFile("departments.txt", 'utf8', function(err, data) {  
+   if (err) throw err;
+    var textByLine = data.split("\n")
+    console.log(textByLine[0]);
+    console.log(textByLine);
+});
