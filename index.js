efb2806137c52f3921577824d17622899b926bae
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-02-18 15:23:58 +0000

    obtained the output of a worker working in the department

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 fs.readFile("departments.txt", 'utf8', function(err, data) {  
    if (err) throw err;
-    var textByLine = data.split("\n")
-    console.log(textByLine[0]);
-    console.log(textByLine);
+   var textByLine = data.split("\n")
+   console.log(textByLine[0]);
+   console.log(textByLine);
 });
